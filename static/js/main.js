1fb5e41d4ad1885df1ebeb7244fd542946b9fbd0
hduss <maintenancedotnet3@gmail.com>
2022-02-01 00:21:42 +0100

    Delete && comment unused code

--- static/js/main.js
+++ static/js/main.js
@@ -1,130 +1,117 @@
+window.addEventListener('load', function(event) {
 
-console.log('app => ', app)
+    console.log('app => ', app)
 
-let linkId = document.querySelector(".link_id");
-let showMore = document.querySelector('#show_more');
-
-
-linkId.addEventListener('click', (e) => {
-    e.preventDefault();
-    console.log('je suis click');
-})
-
-
-document.addEventListener('click', (event) =>  {
-    event.preventDefault();
-	if (!event.target.matches('.link_id')) return;
-
-	console.log('Id cryptos => ', event.target.getAttribute('id'))
-	console.log('Target => ', event.target.closest('tr'));
-	let last_row = document.querySelector('#last_row')
-
-    axios.get('/api/cryptos')
-    .then(function (response) {
-        console.log(response);
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
-
-}, false);
-
-
-showMore.addEventListener('click', function (event) {
-    event.preventDefault();
-	if (!event.target.matches('#show_more')) return;
-
-	let limit = event.target.getAttribute("data-limit")
-	let offset = event.target.getAttribute("data-offset")
-	let table = document.querySelector('#main-tab')
-
-    axios.get('/api/cryptos/' + limit + '/' + offset)
-    .then(function (response) {
-
-        let returnDatas = response['data']['data']
-        console.log('Datas => ', response['data']['data'])
-
-        if(returnDatas.length > 0){
-
-            let trNode = document.createElement("tr");
-            let trValue = "";
-
-            for(let i = 0; i < returnDatas.length; i++){
-
-                let tr = table.insertRow(-1);
-                let tdRank = tr.insertCell()
-                let tdName = tr.insertCell()
-                let tdPrice = tr.insertCell()
-                let tdCurrency = tr.insertCell()
-
-                tdRank.innerHTML = returnDatas[i]['rank']
-                tdName.innerHTML = '<a href="" id="' + returnDatas[i]['id'] + '" class="link_id">' + returnDatas[i]['name'] + '</a>'
-                tdPrice.innerHTML = returnDatas[i]['priceUsd']
-                tdCurrency.innerHTML = 'USD'
-            }
-
-//            let newLimit = parseInt(limit) + 10
-            let newOffset = parseInt(offset) + 10
-            event.target.setAttribute("data-offset", newOffset)
-
-        }else{
-            console.log('Fail get datas')
+    new Vue({
+        el: '#app',
+        delimiters : ['[[', ']]'],
+    
+        data: {
+            infos: null 
+        },
+        mounted () {
+            axios
+            .get('/api/coins')
+            .then((response) => {
+                console.log('response vue => ', response.data.data)
+                this.infos = response.data.data
+            })
         }
     })
-    .catch(function (error) {
-        console.log(error);
-    });
 
-}, false);
+    let linkId = document.querySelector(".link_id");
+    let showMore = document.querySelector('#show_more');
 
 
- document.addEventListener('click',(e) => {
-    if(e.target && e.target.id== 'brnPrepend'){
-          //do something
-     }
+    linkId.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log('je suis click');
+    })
 
-     console.log('je suis cliek after')
- });
 
-axios
-.get('/api/coins')
-.then((response) => {
-    console.log('response => ', response)
-})
-.catch((err) => {
-    console.log('error ici')
-})
+    document.addEventListener('click', (event) =>  {
+        event.preventDefault();
+        if (!event.target.matches('.link_id')) return;
 
- new Vue({
-    el: '#app',
-    delimiters : ['[[', ']]'],
-    data: {
-        info: null 
-    },
-    mounted () {
-        axios
-        .get('/api/coins')
-        .then((response) => {
-            console.log('response vue => ', response.data.data)
-            this.info = response.data.data
+        console.log('Id cryptos => ', event.target.getAttribute('id'))
+        console.log('Target => ', event.target.closest('tr'));
+        let last_row = document.querySelector('#last_row')
+
+        axios.get('/api/cryptos')
+        .then(function (response) {
+            console.log(response);
         })
-    }
-})
+        .catch(function (error) {
+            console.log(error);
+        });
+
+    }, false);
 
 
+    showMore.addEventListener('click', function (event) {
+        event.preventDefault();
+        if (!event.target.matches('#show_more')) return;
 
-//axios.get('/api/cryptos')
-//
-//.then(function (response) {
-//
-//    const tab = document.getElementById('main-tab');
-//    console.log('Tab ici =>', tab);
-//    console.log('Tab children => ', tab.children);
-//
-//
-//
-//    console.log(response);
-//})
-//.catch(function (error) {
-//    console.log(error);
-//});
+        let limit = event.target.getAttribute("data-limit")
+        let offset = event.target.getAttribute("data-offset")
+        let table = document.querySelector('#main-tab')
+
+        axios.get('/api/cryptos/' + limit + '/' + offset)
+        .then(function (response) {
+
+            let returnDatas = response['data']['data']
+            console.log('Datas => ', response['data']['data'])
+
+            if(returnDatas.length > 0){
+
+                let trNode = document.createElement("tr");
+                let trValue = "";
+
+                for(let i = 0; i < returnDatas.length; i++){
+
+                    let tr = table.insertRow(-1);
+                    let tdRank = tr.insertCell()
+                    let tdName = tr.insertCell()
+                    let tdPrice = tr.insertCell()
+                    let tdCurrency = tr.insertCell()
+
+                    tdRank.innerHTML = returnDatas[i]['rank']
+                    tdName.innerHTML = '<a href="" id="' + returnDatas[i]['id'] + '" class="link_id">' + returnDatas[i]['name'] + '</a>'
+                    tdPrice.innerHTML = returnDatas[i]['priceUsd']
+                    tdCurrency.innerHTML = 'USD'
+                }
+
+    //            let newLimit = parseInt(limit) + 10
+                let newOffset = parseInt(offset) + 10
+                event.target.setAttribute("data-offset", newOffset)
+
+            }else{
+                console.log('Fail get datas')
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+    }, false);
+
+
+    // document.addEventListener('click',(e) => {
+    // if(e.target && e.target.id== 'brnPrepend'){
+    //         //do something
+    //     }
+
+    //     console.log('je suis click after')
+    // });
+
+    // axios
+    // .get('/api/coins')
+    // .then((response) => {
+    // console.log('response => ', response)
+    // })
+    // .catch((err) => {
+    // console.log('error ici')
+    // })
+
+
+});
